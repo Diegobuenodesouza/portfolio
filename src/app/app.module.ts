dd46e5002a65c54965170c00fbc2112d76f307e4
Diego Souza <diegobueno.souza@gmail.com>
2021-01-14 23:06:08 -0300

    Inicio Portfolio
    Inicio Portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavegacaoComponent } from './navegacao/navegacao.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ReferenciasComponent } from './referencias/referencias.component';
+import { TrabalhoComponent } from './trabalho/trabalho.component';
+;
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavegacaoComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ReferenciasComponent,
+    TrabalhoComponent
+    ],
   imports: [
     BrowserModule
   ],
